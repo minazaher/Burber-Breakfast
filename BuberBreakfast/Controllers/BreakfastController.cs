03220d2f68fb86f7a4362400bcc5c370c1c02938
minazaher <menazaher115@gmail.com>
2024-03-30 11:18:00 +0200

    first commit

---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,36 @@
+using BuberBreakfast.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("breakfasts")]
+public class BreakfastController : ControllerBase
+{
+
+    [HttpPost()]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+    
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateBreakfast(Guid id, UpdateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+    
+}
